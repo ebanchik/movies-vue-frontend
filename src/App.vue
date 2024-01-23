ef131f9373653736b55ad0c21a1c860bf56d8a2c
ebanchik <ebanchik@gmail.com>
2024-01-23 13:37:26 -0500

    index action

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,38 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+  import axios from "axios";
+  export default {
+    data: function () {
+      return {
+        movies: [],
+        newMovieParams: {},
+      };
+    },
+    created: function () {
+      this.indexMovies();
+    },
+    methods: {
+      indexMovies: function () {
+        axios.get("/movies.json").then((response) => {
+          console.log("movies index", response);
+          this.movies = response.data;
+        });
+      },
+    },
+  };
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="home">
+    <h1>All Movies</h1>
+    <div v-for="movie in movies" v-bind:key="movie.id">
+      <h2>{{ movie.title }}</h2>
+      <img v-bind:src="movie.url" v-bind:alt="movie.title" />
+      <p>Director: {{ movie.director }}</p>
+      <p>Genre: {{ movie.genre }}</p>
+      <p>Runtime: {{ movie.runtime }}</p>
+      <p>Rating: {{ movie.rating }}</p>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style></style>
